5646860ddd69cd0b534d500855ee5f794e746573
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-06-28 12:43:22 -0500

    commit tree ca3159325cc06e91bdff69936d340b5c55dad0c8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validate :clickbait
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  def clickbait
+    clicky_titles = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    unless title&& title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+      errors.add(:title, "Not clickbait-y enough!")
+    end
+  end
 end
